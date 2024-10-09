bd2f4716f9e898c5783b21696f9c6fbafdc70d81
Kevin Lubojacky <klubo@DESKTOP-P8AC8G4.localdomain>
2024-10-09 12:00:43 -0600

    First Push

---
+++ m1part1.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculate the addition
+print (num1, '+', num2, 'is', num1 + num2)
+
+# Calculate the subtraction
+print (num1, '-', num2, 'is', num1 - num2)
---
+++ m1part2.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculate the multiplication
+print (num1, '*', num2, 'is', num1 * num2)
+
+# Calculate the division
+print (num1, '/', num2, 'is', num1 / num2)
